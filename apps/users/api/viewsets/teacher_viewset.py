f32219e956dda2b5ed698175f76067cd0bae3256
Mauricio Medina <mau172001mece@gmail.com>
2024-11-27 00:04:24 -0600

    cambien a buscar grupos por cursos y agregue un accion para buscar y otro para crear varios usuarios a partir de una lista

--- apps/users/api/viewsets/teacher_viewset.py
+++ apps/users/api/viewsets/teacher_viewset.py
@@ -6,11 +6,11 @@ from rest_framework.response import Response
 from rest_framework.decorators import action
 
 from apps.users.models import User
+from apps.groups.api.serializers.course_serializer import CourseSerializer
 from apps.users.api.serializers.teacher_serializer import (
     TeacherSerializer, TeacherListSerializer, 
     PasswordSerializer, UpdateTeacherSerializer,
 )
-from apps.groups.api.serializers.group_serializer import ClassGroupSerializer
 
 class TeacherViewSet(viewsets.GenericViewSet):
     model = User
@@ -25,37 +25,69 @@ class TeacherViewSet(viewsets.GenericViewSet):
         if self.queryset is None:
             self.queryset = self.model.objects\
                             .filter(is_active=True, rol='TEACHER')\
-                            .values('id', 'username', 'email', 'name',)
+                            .values('id', 'username', 'email', 'name','last_name')
         return self.queryset
     
-    def get_groups_by_teacher(self, teacher=None):
+    def get_courses_by_teacher(self, teacher=None):
         if teacher is not None:
-            return ClassGroupSerializer.Meta.model.objects.filter(state=True, teacher=teacher)
+            return CourseSerializer.Meta.model.objects.filter(state=True, teacher=teacher)
     
     @action(detail=True, methods=['get'])
-    def get_groups(self, request, pk=None):
+    def get_courses(self, request, pk=None):
         teacher = self.get_object(pk)
 
         if teacher:
-            groups = self.get_groups_by_teacher(teacher)
-            groups_serializer = ClassGroupSerializer(groups, many=True)
-            return Response({"groups" : groups_serializer.data})
+            course = self.get_courses_by_teacher(teacher)
+            course_serializer = CourseSerializer(course, many=True)
+            return Response({"cursos" : course_serializer.data})
         return Response({
             'message': 'Hay errores en la información enviada',
-            'errors': 'No existe el docente'
+            'errors': 'No existe el grupo'
         }, status=status.HTTP_400_BAD_REQUEST)
+    
+    @action(detail=False, methods=['post'],)
+    def create_from_list(self, request):
+        """
+        Action que acepta una lista de usuarios para crear múltiples instancias.
+        """
+        data = request.data  # La lista de objetos JSON enviada por el cliente
+        
+        if not isinstance(data, list):
+            return Response({"error": "Se esperaba una lista de docentes."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        success = []
+        errors = []
+
+        for user in data:
+            user_serializer = self.serializer_class(data=user)
+            if user_serializer.is_valid():
+                user_serializer.save()
+                success.append({
+                    'message': 'Usuario creado correctamente.',
+                    'user': user_serializer.data
+                })
+            else:
+                errors.append({
+                    'user': user['username'],
+                    'errors': user_serializer.errors
+                })
+        
+        return Response({
+            'success': success,
+            'errors': errors
+        }, status=status.HTTP_207_MULTI_STATUS if errors else status.HTTP_201_CREATED)
     
     @action(methods=['get'], detail=False)
     def search_teacher(self, request):
         username_or_name = request.query_params.get('username_or_name', '')
-        student = User.objects.filter(
+        teacher = User.objects.filter(
             Q(username__icontains=username_or_name)|
             Q(name__icontains=username_or_name),
             rol = 'TEACHER'
         )
-        if student:
-            student_serializer = self.serializer_class(student, many=True)
-            return Response(student_serializer.data, status=status.HTTP_200_OK)
+        if teacher:
+            teacher_serializer = self.serializer_class(teacher, many=True)
+            return Response(teacher_serializer.data, status=status.HTTP_200_OK)
         return Response({
             'mensaje': 'No se ha encontrado un Docente.'
         }, status=status.HTTP_400_BAD_REQUEST)
@@ -85,7 +117,8 @@ class TeacherViewSet(viewsets.GenericViewSet):
         if user_serializer.is_valid():
             user_serializer.save()
             return Response({
-                'message': 'Usuario registrado correctamente.'
+                'message': 'Usuario registrado correctamente.',
+                'user': user_serializer.data
             }, status=status.HTTP_201_CREATED)
         return Response({
             'message': 'Hay errores en el registro',
